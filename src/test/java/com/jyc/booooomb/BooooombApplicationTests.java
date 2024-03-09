1805442e79bcc6da1f255b05ea7262857aa44403
coderclcl <gattenmaster@gmail.com>
2024-03-10 02:32:52 +0900

    start spring

---
+++ src/test/java/com/jyc/booooomb/BooooombApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyc.booooomb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooooombApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
