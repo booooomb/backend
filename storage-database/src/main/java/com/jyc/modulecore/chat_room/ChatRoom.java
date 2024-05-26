0d27d94bcfecbf3c1c771de304d06eda5bb57d57
jplee227 <jplee227@gmail.com>
2024-05-26 13:35:17 +0900

    키워드, 채팅방, 메시지 엔티티 초기 설계(#5) - 이슈에서 논의된 스키마에 따라 jpa entity 및 repository 를 작성합니다. - api의 도메인에서 해당 repository 의존성을 가지고 사용하게 될 예정입니다.

---
+++ storage-database/src/main/java/com/jyc/modulecore/chat_room/ChatRoom.java
@@ -0,0 +1,37 @@
+package com.jyc.modulecore.chat_room;
+
+import com.jyc.modulecore.keyword.Keyword;
+import com.jyc.modulecore.message.Message;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+
+@Entity
+@Table(name = "chat_room")
+@Getter
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyword_id")
+    private Keyword keyword;
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<Message> messages;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ storage-database/src/main/java/com/jyc/modulecore/chat_room/ChatRoomRepository.java
@@ -0,0 +1,6 @@
+package com.jyc.modulecore.chat_room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
+}
