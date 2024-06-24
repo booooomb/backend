493fd53979ec409dd3165f71329fd10b20ed622b
coderclcl <gattenmaster@gmail.com>
2024-06-23 21:08:24 -0700

    Docker-based MySQL setup for testing

--- storage-database/src/test/java/com/jyc/modulecore/ModuleCoreApplicationTests.java
+++ storage-database/src/test/java/com/jyc/modulecore/ModuleCoreApplicationTests.java
@@ -1,13 +1,13 @@
 package com.jyc.modulecore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ModuleCoreApplicationTests {
-
-	@Test
-	void contextLoads() {
+@SpringBootApplication
+public class ModuleCoreApplicationTests {
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleCoreApplicationTests.class, args);
 	}
-
 }
